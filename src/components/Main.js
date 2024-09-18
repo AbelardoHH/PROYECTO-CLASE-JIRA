b9612758bb83bf3193521bdec2e2c4cadfc7370f
IrvingYHM <hernandezirving725@gmail.com>
2024-09-18 15:09:28 -0600

    PROYCLASE-3 Creacion de Header

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from "./components/Header";  
 
 const Main = () => {
   return (
+    <>
+    <Header/>
     <main>
       <p>Este es el contenido principal de la aplicación.</p>
     </main>
+    </>
   );
 };
 
