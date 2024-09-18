8201c712155d45a7070aad7f8e38f724743a4fab
abelhh2001 <abelhh2001@gmail.com>
2024-09-18 14:39:47 -0600

    Segundo commit

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main>
+      <p>Este es el contenido principal de la aplicación.</p>
+    </main>
+  );
+};
+
+export default Main;
